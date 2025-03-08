838b1e755fad0b9429cf0e2f52d356f7636400c0
EdroVolt <ahmededrees30@gmail.com>
2025-03-08 17:28:27 +0200

    Add FileUploadModule to ApartmentsModule for file upload support
    - Imported FileUploadModule in ApartmentsModule imports
    - Enables file upload functionality for apartments

--- src/apartments/apartments.module.ts
+++ src/apartments/apartments.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Apartment } from './entities/apartment.entity';
 import { ApartmentsController } from './apartments.controller';
 import { ApartmentsService } from './apartments.service';
+import { FileUploadModule } from '../common/file-upload/file-upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Apartment])],
+  imports: [TypeOrmModule.forFeature([Apartment]), FileUploadModule],
   controllers: [ApartmentsController],
   providers: [ApartmentsService],
   exports: [TypeOrmModule],
